02373b39094456e7e49ccc615ecec7783967074c
Lucas Bartholemy <lucas@bartholemy.com>
2018-10-01 11:24:53 +0200

    Module Image & Screenshot

--- Module.php
+++ Module.php
@@ -9,7 +9,10 @@ use humhub\modules\user\models\User;
 
 class Module extends \humhub\components\Module
 {
-
+    /**
+     * @inheritdoc
+     */
+    public $resourcesPath = 'resources';
     /**
      * @inheritdoc
      */
