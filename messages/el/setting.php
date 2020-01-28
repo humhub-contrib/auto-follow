0e6e2b018be72e55d799c32cdfd43b79d671df57
HumHub Translations <info@humhub.com>
2020-01-28 15:05:28 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/el/setting.php
+++ messages/el/setting.php
@@ -1,7 +1,6 @@
 <?php
-
-return [
-    'Add User' => '',
-    'Choose default spaces or users which are automatically followed by new users.' => '',
-    'Force following also for existing members' => '',
-];
+return array (
+  'Add User' => 'Προσθήκη χρήστη',
+  'Choose default spaces or users which are automatically followed by new users.' => 'Επιλέξτε προεπιλεγμένους χώρους ή χρήστες που ακολουθούνται αυτόματα από νέους χρήστες.',
+  'Force following also for existing members' => 'Ζόρισε να ακολουθεί και τα υπάρχοντα μέλη',
+);
--- messages/pl/setting.php
+++ messages/pl/setting.php
@@ -1,7 +1,6 @@
 <?php
-
-return [
-    'Add User' => '',
-    'Choose default spaces or users which are automatically followed by new users.' => '',
-    'Force following also for existing members' => '',
-];
+return array (
+  'Add User' => 'Dodaj użytkownika',
+  'Choose default spaces or users which are automatically followed by new users.' => 'Wybierz domyślne strefy lub użytkowników którzy będą automatycznie obserwowani przez nowych użytkowników.',
+  'Force following also for existing members' => 'Wprowadź zmiany także dla istniejących członków',
+);
