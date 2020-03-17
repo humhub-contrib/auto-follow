275636da0ecc4fbac6778b3070de35f26960bcf6
HumHub Translations <info@humhub.com>
2020-03-17 09:17:00 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/vi/base.php
+++ messages/vi/base.php
@@ -1,5 +1,4 @@
 <?php
-
-return [
-    '<strong>Auto</strong> follow configuration' => '',
-];
+return array (
+  '<strong>Auto</strong> follow configuration' => 'Cấu hình follow <strong>Tự động</strong>',
+);
--- messages/vi/setting.php
+++ messages/vi/setting.php
@@ -1,7 +1,6 @@
 <?php
-
-return [
-    'Add User' => '',
-    'Choose default spaces or users which are automatically followed by new users.' => '',
-    'Force following also for existing members' => '',
-];
+return array (
+  'Add User' => 'Thêm người dùng',
+  'Choose default spaces or users which are automatically followed by new users.' => 'Chọn diễn đàn hoặc người dùng mặc định mà người dùng mới sẽ tự động follow',
+  'Force following also for existing members' => 'Đồng thời áp đặt following cho các thành viên hiện hữu',
+);
