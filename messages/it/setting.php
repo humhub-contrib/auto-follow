289bedfcd7cab64eaa4c612e977ea2b81314eb22
HumHub Translations <info@humhub.com>
2020-10-30 18:01:28 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/it/setting.php
+++ messages/it/setting.php
@@ -1,7 +1,6 @@
 <?php
-
-return [
-    'Add User' => '',
-    'Choose default spaces or users which are automatically followed by new users.' => '',
-    'Force following also for existing members' => '',
-];
+return array (
+  'Add User' => 'Aggiunti utente',
+  'Choose default spaces or users which are automatically followed by new users.' => 'Scegli gli spazi o gli utenti predefiniti che vengono seguiti automaticamente dai nuovi utenti.',
+  'Force following also for existing members' => 'Forza l\'utente a seguire anche per i membri esistenti',
+);
