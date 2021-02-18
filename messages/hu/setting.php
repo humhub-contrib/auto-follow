03c54f75c2c09547530450fc8217452b605a0d8d
HumHub Translations <info@humhub.com>
2021-02-18 10:29:16 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/hu/setting.php
+++ messages/hu/setting.php
@@ -1,6 +1,6 @@
 <?php
 return array (
   'Add User' => 'Felhasználó hozzáadása',
-  'Choose default spaces or users which are automatically followed by new users.' => 'Válasszon alapértelmezett Space-t vagy felhasználókat, amelyeket új felhasználók követnek automatikusan.',
+  'Choose default spaces or users which are automatically followed by new users.' => 'Válassz alapértelmezett közösséget vagy felhasználókat, amelyeket új felhasználók követnek automatikusan.',
   'Force following also for existing members' => 'Követés kényszerítése a meglévő tagok számára is',
 );
