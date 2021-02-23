c5c897f79b1c98a2aecdac5ce1d445ad95e2fa98
HumHub Translations <info@humhub.com>
2021-02-23 15:56:54 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/pt/base.php
+++ messages/pt/base.php
@@ -1,5 +1,4 @@
 <?php
-
-return [
-    '<strong>Auto</strong> follow configuration' => '',
-];
+return array (
+  '<strong>Auto</strong> follow configuration' => '<strong>Configuração</strong> de seguimento automático',
+);
--- messages/pt/setting.php
+++ messages/pt/setting.php
@@ -1,7 +1,6 @@
 <?php
-
-return [
-    'Add User' => '',
-    'Choose default spaces or users which are automatically followed by new users.' => '',
-    'Force following also for existing members' => '',
-];
+return array (
+  'Add User' => 'Adicionar pessoa',
+  'Choose default spaces or users which are automatically followed by new users.' => 'Escolher espaços padrão ou pessoas que são automaticamente seguidas por novas pessoas.',
+  'Force following also for existing members' => 'Forçar participantes existentes a seguir',
+);
