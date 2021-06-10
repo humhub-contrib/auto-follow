9c16222aaa7e0e54ba527f6ca6eb20dd52d45d86
HumHub Translations <info@humhub.com>
2021-06-10 20:13:17 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/th/base.php
+++ messages/th/base.php
@@ -1,5 +1,4 @@
 <?php
-
-return [
-    '<strong>Auto</strong> follow configuration' => '',
-];
+return array (
+  '<strong>Auto</strong> follow configuration' => '<strong>อัตโนมัติ</strong> ติดตามการกำหนดค่า',
+);
--- messages/th/setting.php
+++ messages/th/setting.php
@@ -1,7 +1,6 @@
 <?php
-
-return [
-    'Add User' => '',
-    'Choose default spaces or users which are automatically followed by new users.' => '',
-    'Force following also for existing members' => '',
-];
+return array (
+  'Add User' => 'เพิ่มผู้ใช้',
+  'Choose default spaces or users which are automatically followed by new users.' => 'เลือกช่องว่างเริ่มต้นหรือผู้ใช้ที่ตามด้วยผู้ใช้ใหม่โดยอัตโนมัติ',
+  'Force following also for existing members' => 'บังคับติดตามสำหรับสมาชิกที่มีอยู่ด้วย',
+);
