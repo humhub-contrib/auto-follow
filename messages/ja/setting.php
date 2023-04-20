4d415741460871c2d30925785e1487b8c6ef760a
HumHub Translations <hello@humhub.com>
2023-04-20 14:51:45 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/ja/setting.php
+++ messages/ja/setting.php
@@ -1,7 +1,6 @@
 <?php
-
-return [
-    'Add User' => '',
-    'Choose default spaces or users which are automatically followed by new users.' => '',
-    'Force following also for existing members' => '',
-];
+return array (
+  'Add User' => 'ユーザーを追加する',
+  'Choose default spaces or users which are automatically followed by new users.' => '新しいユーザーが自動的にフォローするデフォルトのスペースまたはユーザーを選択します。',
+  'Force following also for existing members' => '既存会員にも強制フォローします',
+);
