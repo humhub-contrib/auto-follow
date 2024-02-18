20b09b986b27cad99db1de3847cdfd516353c01f
HumHub Translations <hello@humhub.com>
2024-02-18 18:19:08 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/sr/setting.php
+++ messages/sr/setting.php
@@ -1,7 +1,6 @@
 <?php
-
-return [
-    'Add User' => '',
-    'Choose default spaces or users which are automatically followed by new users.' => '',
-    'Force following also for existing members' => '',
-];
+return array (
+  'Add User' => 'Dodaj korisnika',
+  'Choose default spaces or users which are automatically followed by new users.' => 'Odaberite podrazumevane prostore ili korisnike koje će novi korisnici automatski zapratiti.',
+  'Force following also for existing members' => 'Prisili praćenje i za postojeće korisnike',
+);
