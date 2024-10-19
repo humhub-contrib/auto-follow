00666fa01517808ca1782d5867a26db89f2482a0
HumHub Translations <hello@humhub.com>
2024-10-19 21:45:10 +0000

    Enh: Updated Translations (translate.humhub.org)

---
+++ messages/es-419/base.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    '<strong>Auto</strong> follow configuration' => '',
+    'Add User' => '',
+    'Choose default spaces or users which are automatically followed by new users.' => '',
+    'Force following also for existing members' => '',
+];
